0aa194b7db96df84ae33d8af8553513881920fe6
kenancibooglu <kenancibooglu24@gmail.com>
2024-04-20 21:23:58 +0300

    continue

--- src/global/TopBar/TopBar.jsx
+++ src/global/TopBar/TopBar.jsx
@@ -11,35 +11,38 @@ function TopBar() {
 
   return (
     <div className="App">
-      <nav className="bg-gray-40 text-black p-2 top-0 left-0 right-0 z-50 flex navbar">
-        <a className="flex items-center p-2" href="/">
-          <img src={logo} alt="Logo" className="mr-2 logo-png" /> Pick
-          <span className="font-bold">Bazar</span>
-        </a>
-        <a className="flex items-center p-2" href="/shops">
-          Shops
-        </a>
-        <a className="flex items-center p-2" href="/offers">
-          Offers
-        </a>
-        <a className="flex items-center p-2" href="/contanct">
-          Contanct
-        </a>
-        <a onClick={pagesClick} className="flex items-center p-2" href="/pages">
-          Pages <FaAngleDown />
-        </a>
-       
-        <form class="ml-auto py-3">
-          <label
-            for="default-search"
-            class="mb-2 text-sm font-medium text-gray-900 sr-only"
-          >
+      <nav className="bg-gray-40 text-black p-2 top-0 left-0 right-0 z-50 flex flex-col sm:flex-row sm:flex-wrap items-center navbar">
+        <div className="flex items-center p-2">
+          <a href="/" className="flex items-center">
+            <img src={logo} alt="Logo" className="mr-2 logo-png" /> Book
+            <span className="font-bold">Market</span>
+          </a>
+        </div>
+        <div className="flex flex-col sm:flex-row sm:flex-wrap">
+          <a href="/shops" className=" items-center p-2 hidden sm:flex">
+            Shops
+          </a>
+          <a href="/offers" className=" items-center p-2 hidden sm:flex">
+            Offers
+          </a>
+          <a href="/contanct" className=" items-center p-2 hidden sm:flex">
+            Contact
+          </a>
+          <div className="relative  items-center p-2 hidden sm:flex">
+            <a onClick={pagesClick} className="flex items-center" href="/pages">
+              Pages <FaAngleDown />
+            </a>
+          </div>
+        </div>
+        {/* Search */}
+        <form className="relative flex items-center ml-auto">
+          <label htmlFor="default-search" className="mb-2 text-sm  font-medium text-gray-900 sr-only">
             Search
           </label>
-          <div class="relative">
-            <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+          <div className="relative">
+            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
               <svg
-                class="w-4 h-4 text-gray-800 dark:text-gray-900"
+                className="w-4 h-4 text-gray-800 dark:text-gray-900"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -47,9 +50,9 @@ function TopBar() {
               >
                 <path
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
                 />
               </svg>
@@ -57,25 +60,30 @@ function TopBar() {
             <input
               type="search"
               id="default-search"
-              className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-900 rounded-lg bg-gray-900 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-100 dark:border-gray-600 dark:placeholder-gray-800 dark:text-gray-800 inputLine "
+              className="w-full p-4 ps-10 text-sm text-gray-900 border border-gray-900 rounded-lg bg-gray-900 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-100 dark:border-gray-600 dark:placeholder-gray-800 dark:text-gray-800 inputLine hidden sm:block"
               placeholder="Search"
               required
             />
+
           </div>
         </form>
-        <a href="/shopping">
+        <a href="/shopping" className="p-2 hidden sm:flex">
           <FaShoppingCart className="shopping" />
         </a>
-        <button class="bg-green-700 hover:bg-green-900 text-white font-bold px-3 border  rounded h-10 m-5">
-          Join
-        </button>
-        <a href="/" class="bg-green-700 hover:bg-green-900 text-white font-bold px-5 border  rounded h-10 m-5 flex justify-center items-center">
-          Become a Seller
-        </a>
-       
+
+        <div className="flex flex-col sm:flex-row sm:flex-wrap">
+        <a href="/" className="bg-green-700 hover:bg-green-900 text-white font-bold px-5 border rounded h-10 m-2 flex justify-center items-center">
+            Join
+          </a>
+          <a href="/" className="bg-green-700 hover:bg-green-900 text-white font-bold px-5 border rounded h-10 m-2 flex justify-center items-center">
+            Become a Seller
+          </a>
+        </div>
       </nav>
-      
     </div>
+
+
+
   );
 }
 
